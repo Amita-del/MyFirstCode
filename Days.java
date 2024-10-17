b8cbd379cb83af91874e37481317825908918abb
Amita-Patel <amipatel2341@gmail.com>
2024-10-17 20:23:28 +0530

    Add files via upload

---
+++ Days.java
@@ -0,0 +1,38 @@
+import Java.util.*;
+public class Days{
+	public static void main(string[] args)
+	{
+Scanner Y=new scanner(system.in);
+System.out.println("Enter number of days");
+int a=y.nextInt( );
+if(a==1)
+{
+	System.out.println("Sunday");
+}
+else if(a==2)
+{
+	System.out.println("Monday");
+}
+else if(a==3)
+{
+	System.out.println("Tuesday");
+}
+else if(a==4)
+{
+	System.out.println("wednesday");
+}
+else if(a==5)
+{
+	System.out.println("Thrusday");
+}
+else if(a==6)
+{
+	system.out.println("Friday");
+}
+else
+{
+	System.out.println("Saturday");
+}
+	}
+}
+}
---
+++ While.java
@@ -0,0 +1,7 @@
+class While {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
